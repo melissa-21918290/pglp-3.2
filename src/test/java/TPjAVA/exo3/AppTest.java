4d6e20f12337eb39f7b9ae6027a885213749818b
melissa bafdel <melissabafdel@gmail.com>
2020-03-10 15:39:16 +0100

    modif

--- src/test/java/TPjAVA/exo3/AppTest.java
+++ src/test/java/TPjAVA/exo3/AppTest.java
@@ -31,7 +31,7 @@ public class AppTest {
 	@Test
 	public final void test_salaireTotal() {
 		emp = new employe(500,2030,2000);
-	    assertFalse(emp.salaire_total()==23344);
+	    assertFalse(emp.salaire_total()==2344);
 	}
 	
 	
