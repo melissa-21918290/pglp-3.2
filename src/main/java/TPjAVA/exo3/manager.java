2e07a671464a0c3b14ab57fd4643b733ab5353be
melissa bafdel <melissabafdel@gmail.com>
2020-03-10 15:26:31 +0100

    ajout*

--- src/main/java/TPjAVA/exo3/manager.java
+++ src/main/java/TPjAVA/exo3/manager.java
@@ -2,6 +2,7 @@ package TPjAVA.exo3;
 
 public class manager extends employe {
 	
+	private int nbordres=5; 
 	public manager(int salaire, int annee_courant, int annee_entree) {
 		super(salaire, annee_courant, annee_entree);
 	}
--- src/test/java/TPjAVA/exo3/AppTest.java
+++ src/test/java/TPjAVA/exo3/AppTest.java
@@ -8,6 +8,7 @@ public class AppTest {
 	
 	employe emp;
 	vendeur ven;
+	manager man; 
 	@Test
 	public final void test_salaireEmp() {
 		emp = new employe(1700,2030,2000);
@@ -29,9 +30,15 @@ public class AppTest {
 	
 	@Test
 	public final void test_salaireTotal() {
-		emp = new employe(500,2030,20000);
+		emp = new employe(500,2030,2000);
 	    assertFalse(emp.salaire_total()==23344);
 	}
-
+	
+	
+	@Test
+	public final void test_salaire_Man() {
+		man = new manager(1570,2030,2000);
+	    assertTrue(man.salaire_Man(5)==2020);
+	}
 
 }
